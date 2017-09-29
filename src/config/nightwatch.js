4dd65e2b0f70af471d84f79e2eac708b65470ca8
airt <airtial@gmail.com>
2017-09-29 11:03:52 +0800

    fix: nightwatch custom paths

--- src/config/nightwatch.js
+++ src/config/nightwatch.js
@@ -2,17 +2,22 @@
 
 require('../utils/register')
 
+const fs = require('fs')
 const chromedriver = require('chromedriver')
 const seleniumServer = require('selenium-server')
 const { dev: config } = require('../config')
 const { projectPath } = require('../utils/paths')
 
+const customCommandsPath = [projectPath('test/e2e/commands')].filter(fs.existsSync)
+
+const customAssertionsPath = [projectPath('test/e2e/assertions')].filter(fs.existsSync)
+
 // http://nightwatchjs.org/gettingstarted#settings-file
 module.exports = {
   src_folders: [projectPath('test/e2e/specs')],
   output_folder: projectPath('test/e2e/reports'),
-  custom_commands_path: [projectPath('node_modules/nightwatch-helpers/commands'), projectPath('test/e2e/commands')],
-  custom_assertions_path: [projectPath('node_modules/nightwatch-helpers/assertions'), projectPath('test/e2e/assertions')],
+  custom_commands_path: [projectPath('node_modules/nightwatch-helpers/commands')].concat(customCommandsPath),
+  custom_assertions_path: [projectPath('node_modules/nightwatch-helpers/assertions')].concat(customAssertionsPath),
 
   selenium: {
     start_process: true,
